65abc8278ec86d8284ea1b14e145cd9251a69cc5
Oğuzhan Sönmeztürk <oguzhansonmezturk@gmail.com>
2024-11-07 18:55:39 +0300

    First Commit

---
+++ Grade Calculator/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Grade Calculator
+//
+//  Created by Oğuzhan Sönmeztürk on 7.11.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Grade Calculator/Grade_CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Grade_CalculatorApp.swift
+//  Grade Calculator
+//
+//  Created by Oğuzhan Sönmeztürk on 7.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct Grade_CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
